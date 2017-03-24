7e343d4f7ae2dc5a80bb229638f647af5a27209f
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-03-24 17:01:52 -0500

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
@@ -12,8 +13,6 @@ export default {
 		'nti-lib-dom',
 		'nti-lib-interfaces',
 		'nti-util-logger',
-		'nti-web-client',
-		'uuid',
 	],
 	plugins: [
 		babel({ exclude: 'node_modules/**' }),
